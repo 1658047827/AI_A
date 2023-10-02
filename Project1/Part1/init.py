9b8784584023f14f10cb90a2db8a54e828ef9210
Senyu Xie <1658047827@qq.com>
2023-10-02 15:57:47 +0800

    ✨ feat: 完成sin拟合数据的生成

--- Project1/Part1/init.py
+++ Project1/Part1/init.py
@@ -18,13 +18,21 @@ def seed_everything(seed=42):
 def data_generator(data_path):
     """
     生成 f(x)=sin(x) 的训练数据。
-    
+
     参数:
     - data_path (str): 将生成的数据保存到该路径。
     """
     x = np.random.rand(10000, 1) * 2 * np.pi - np.pi  # [-pi, pi)
     y = np.sin(x)
-    raise NotImplementedError
+    np.savez(
+        data_path,
+        x_train=x[:8000],
+        y_train=y[:8000],
+        x_valid=x[8000:9000],
+        y_valid=y[8000:9000],
+        x_test=x[9000:],
+        y_test=y[9000:],
+    )
 
 
 def data_preprocess():
