641519f14f4e94ae9840f4d0c9d80b956af1bcee
Senyu Xie <1658047827@qq.com>
2023-10-03 14:44:18 +0800

    ✨ feat: 完成DataLoader

--- Project1/Part1/utils.py
+++ Project1/Part1/utils.py
@@ -46,8 +46,11 @@ class RandomSampler(Sampler):
 
         返回:
         - (Generator[int]): 通过 yield 关键字返回生成器对象，生成的索引来自 shuffle 后的结果。
+
+        注意:
+        - 每次调用都会刷新一次随机索引序列。
         """
-        yield from np.random.permutation(self.num_samples)
+        yield from np.random.permutation(self.num_samples).tolist()
 
     def __len__(self):
         return self.num_samples
@@ -61,18 +64,23 @@ class BatchSampler(Sampler):
 
     def __iter__(self):
         """
-        
+        返回一个实现了 __next__ 方法的对象。
+
+        返回:
+        - (Generator[list]): 通过 yield 关键字返回生成器对象，生成的索引列表元素通过迭代 sampler 获得。
+
+        注意:
+        - for idx in self.sampler 中会隐式地 sampler_iter = iter(self.sampler) \
+            然后不断地 next(sampler_iter) 获取元素。
         """
-        # 使用 iter() 函数创建 sampler 的迭代器
-        sampler_iter = iter(self.sampler)
-        while True:
-            batch = [next(sampler_iter) for _ in range(self.batch_size)]
+        batch = []
+        for idx in self.sampler:
+            batch.append(idx)
             if len(batch) == self.batch_size:
                 yield batch
-            elif len(batch) > 0 and not self.drop_last:
-                yield batch
-            else:
-                break
+                batch = []
+        if len(batch) > 0 and not self.drop_last:
+            yield batch
 
     def __len__(self):
         if self.drop_last:
@@ -86,7 +94,7 @@ def default_collate(batch):
     默认数据整理函数，将数据打包成第一维是 batch_size 的 ndarray 。
 
     参数:
-    - batch (list): 其中的 ndarray 必须形状相同。
+    - batch (Sequence): 其中的 ndarray 必须形状相同。
 
     返回:
     - stacked (ndarray): 打包后的 ndarray 。如果 batch 中的元素形状为 (d_1, d_2, ...) \
@@ -108,7 +116,9 @@ def default_collate(batch):
 
 
 class DataLoader:
-    def __init__(self, dataset, batch_size=1, shuffle=False, collate_fn=None, drop_last=False):
+    def __init__(
+        self, dataset, batch_size=1, shuffle=False, collate_fn=None, drop_last=False
+    ):
         sampler = RandomSampler(dataset) if shuffle else SequentialSampler(dataset)
         batch_sampler = BatchSampler(sampler, batch_size, drop_last)
         if collate_fn is None:
@@ -119,20 +129,14 @@ class DataLoader:
         self.drop_last = drop_last
         self.sampler = sampler
         self.batch_sampler = batch_sampler
+        self.batch_sampler_iter = None
         self.collate_fn = collate_fn
 
+    def __len__(self):
+        return len(self.batch_sampler)
+
     def __next__(self):
-        """
-
-        
-        返回:
-        - 
-
-        注意:
-        - 
-        """
-        batch_sampler_iter = iter(self.batch_sampler)
-        indices = next(batch_sampler_iter)
+        indices = next(self.batch_sampler_iter)
         data = [self.dataset[idx] for idx in indices]
         stacked = self.collate_fn(data)
         return stacked
@@ -143,11 +147,12 @@ class DataLoader:
 
         返回:
         - self (DataLoader): 对象本身。
-        
+
         注意:
         - 返回自身，因为自身已经实现了 __next__ 。
+        - 在这里调用 iter 创建 BatchSampler 的迭代器。每次 for batch in dataloader \
+            其实隐式地调用 iter(dataloader) 同时执行 iter(self.batch_sampler) ，也就是 \
+            开始进行一轮新的采样。
         """
+        self.batch_sampler_iter = iter(self.batch_sampler)
         return self
-
-def func():
-    yield from [1,2,3]
