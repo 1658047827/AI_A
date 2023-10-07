aef76b1566996c4d4709eaa2817b2529a6ca0006
Senyu Xie <1658047827@qq.com>
2023-10-07 10:43:19 +0800

    ✨ feat: 完成汉字分类器类

--- Project1/Part1/model.py
+++ Project1/Part1/model.py
@@ -2,8 +2,18 @@ import os
 import logging
 import time
 import pickle
+import numpy as np
 from optim import Optimizer, StepLR
-from nn import Module, Linear, Sigmoid, ReLU, LeakyReLU, MSELoss
+from nn import (
+    Module,
+    Linear,
+    Sigmoid,
+    ReLU,
+    LeakyReLU,
+    Softmax,
+    MSELoss,
+    CrossEntropyLoss,
+)
 
 
 class ANN(Module):
@@ -19,7 +29,7 @@ class ANN(Module):
         for i in range(len(layer_sizes) - 2):
             self.module_list.append(Linear(layer_sizes[i], layer_sizes[i + 1]))
             # 若用 ReLU 激活，学习率得调小，不然容易出现神经元死亡
-            self.module_list.append(Sigmoid())
+            self.module_list.append(ReLU())
         self.module_list.append(Linear(layer_sizes[-2], layer_sizes[-1]))
 
     def forward(self, inputs):
@@ -43,9 +53,8 @@ class MLP:
         valid_loss = 0.0
         for batch in valid_loader:
             predicts = self.model(batch["x"])
-            train_loss = criterion(predicts, batch["y"])
-            valid_loss += train_loss
-        valid_loss = valid_loss / len(valid_loader)
+            valid_loss += criterion(predicts, batch["y"])
+        valid_loss /= len(valid_loader)
         result["valid_loss"] = valid_loss
 
         if metric is not None:
@@ -67,13 +76,12 @@ class MLP:
             epoch_loss = 0.0
             for batch in train_loader:
                 predicts = self.model(batch["x"])
-                train_loss = loss_fn(predicts, batch["y"])
-                epoch_loss += train_loss
+                epoch_loss += loss_fn(predicts, batch["y"])
                 grads = loss_fn.backward()
                 self.model.backward(grads)
                 optimizer.step()
                 optimizer.zero_grad()
-            epoch_loss = epoch_loss / len(train_loader)
+            epoch_loss /= len(train_loader)
             epoch_time_elapsed = time.time() - epoch_time_start
             if epoch % log_interval == 0:
                 logging.info(
@@ -92,21 +100,128 @@ class MLP:
                 )
                 result["train_loss"] = epoch_loss
                 self.best = result
-                self.save_model(save_path)
+                if save_path is not None:
+                    self.save_model(save_path)
 
             # scheduler.step()
-        self.load_model(save_path)
+        if save_path is not None:
+            self.load_model(save_path)
 
     def predict(self, inputs):
         return self.model(inputs)
 
     def save_model(self, save_path="./model.pkl"):
-        """
-        同时保存 self.model 和 self.best 。
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        with open(save_path, "wb") as file:
+            pickle.dump({"model": self.model, "best": self.best}, file)
+        logging.info("Save model to {}".format(save_path))
 
-        参数:
-        - save_path (str, optional): 对象序列化后保存到的文件的路径。
-        """
+    def load_model(self, load_path="./model.pkl"):
+        with open(load_path, "rb") as file:
+            model_dict = pickle.load(file)
+            self.model = model_dict["model"]
+            self.best = model_dict["best"]
+        logging.info("Load model from {}".format(load_path))
+
+
+def accuracy(predicts, labels):
+    predicts = np.argmax(predicts, axis=1, keepdims=False)
+    acc = np.mean(predicts == labels)
+    return acc
+
+
+class MLPClassifier:
+    def __init__(self, input_dim, output_dim):
+        layer_sizes = [input_dim, 1024, 2048, 512, output_dim]
+        # 若使用 CrossEntropyLoss 作为损失函数，则模型最后不需要加 Softmax
+        self.model = ANN(layer_sizes)  # 激活函数用 ReLU 效果好一点
+        self.best = {"train_loss": float("inf"), "valid_loss": float("inf")}  # 记录最佳结果
+
+    def evaluate(self, valid_loader, criterion, metric):
+        result = {}
+        valid_loss = 0.0
+        valid_acc = 0.0
+        for batch in valid_loader:
+            predicts = self.model(batch["x"])
+            valid_loss += criterion(predicts, batch["y"])
+            valid_acc += metric(predicts, batch["y"])
+        valid_loss /= len(valid_loader)
+        valid_acc /= len(valid_loader)
+        result["valid_loss"] = valid_loss
+        result["valid_acc"] = valid_acc
+        return result
+
+    def fit(
+        self, train_loader, valid_loader, epoches, learning_rate, save_path, **kwargs
+    ):
+        optimizer = Optimizer(self.model, learning_rate)
+        # scheduler = StepLR(optimizer, 1000, 0.5)
+        loss_fn = CrossEntropyLoss()
+        metric = accuracy
+        log_interval = kwargs.get("log_interval", 10)
+
+        for epoch in range(1, epoches + 1):
+            epoch_time_start = time.time()
+            epoch_loss = 0.0
+            epoch_acc = 0.0
+            for batch in train_loader:
+                predicts = self.model(batch["x"])
+                epoch_loss += loss_fn(predicts, batch["y"])
+                epoch_acc += metric(predicts, batch["y"])
+                grads = loss_fn.backward()
+                self.model.backward(grads)
+                optimizer.step()
+                optimizer.zero_grad()
+            epoch_loss /= len(train_loader)
+            epoch_acc /= len(train_loader)
+            epoch_time_elapsed = time.time() - epoch_time_start
+            if epoch % log_interval == 0:
+                logging.info(
+                    "Train epoch {}/{}, learning rate: {}, train loss: {:.6f}, train_acc: {:.6f} [{:.2f}s]".format(
+                        epoch,
+                        epoches,
+                        optimizer.lr,
+                        epoch_loss,
+                        epoch_acc,
+                        epoch_time_elapsed,
+                    )
+                )
+
+            # evaluate
+            result = self.evaluate(valid_loader, loss_fn, metric)
+            logging.info(
+                "valid_acc: {:.6f}, valid_loss: {:.6f}".format(
+                    result["valid_acc"], result["valid_loss"]
+                )
+            )
+            if result["valid_acc"] > self.best["valid_acc"]:
+                logging.info(
+                    "Best validation accuracy has been updated: {:.6f} -> {:.6f}".format(
+                        self.best["valid_acc"], result["valid_acc"]
+                    )
+                )
+                result["train_loss"] = epoch_loss
+                result["train_acc"] = epoch_acc
+                self.best = result
+                if save_path is not None:
+                    self.save_model(save_path)
+
+            # scheduler.step()
+        if save_path is not None:
+            self.load_model(save_path)
+
+    def predict(self, inputs, show_prob=False):
+        result = {}
+        predicts = self.model(inputs)
+        pred = np.argmax(predicts, axis=1, keepdims=False)
+        result["pred"] = pred
+        if show_prob:
+            softmax = Softmax(dim=1)
+            prob = softmax(predicts)
+            result["prob"] = prob
+        return result
+
+    def save_model(self, save_path="./model.pkl"):
         os.makedirs(os.path.dirname(save_path), exist_ok=True)
         with open(save_path, "wb") as file:
             pickle.dump({"model": self.model, "best": self.best}, file)
