ba4250de3b95ada40fa5984dfa1e067d8b41bf5f
Senyu Xie <1658047827@qq.com>
2023-11-25 20:13:41 +0800

    ✨ feat: HMM

---
+++ Project2/Part1/HMM.py
@@ -0,0 +1,102 @@
+import numpy as np
+from tqdm import tqdm
+
+
+class HMM_model:
+    def __init__(self, tag2idx, vocab=None):
+        self.tag2idx = tag2idx
+        self.n_tag = len(self.tag2idx)  # 标签（隐状态）个数
+        self.vocab = vocab
+        self.epsilon = 1e-100
+        self.idx2tag = {idx: tag for (tag, idx) in self.tag2idx.items()}
+        self.A = np.zeros((self.n_tag, self.n_tag))
+        self.B = np.zeros((self.n_tag, len(vocab)))
+        self.Pi = np.zeros(self.n_tag)
+
+    def train(self, train_data):
+        for sentence, labels in tqdm(train_data):
+            for j in range(len(sentence)):
+                cur_word = sentence[j]
+                cur_tag = labels[j]
+                self.B[self.tag2idx[cur_tag]][self.vocab[cur_word]] += 1
+                if j == 0:  # 统计初始概率
+                    self.Pi[self.tag2idx[cur_tag]] += 1
+                    continue
+                pre_tag = labels[j - 1]
+                self.A[self.tag2idx[pre_tag]][self.tag2idx[cur_tag]] += 1
+
+        # 对数据进行对数归一化，并最后取log防止连乘浮点数下溢
+        self.Pi = self.Pi / self.Pi.sum()
+        self.Pi[self.Pi == 0.0] = self.epsilon
+        self.Pi = np.log10(self.Pi)
+
+        row_sums = self.A.sum(axis=1)
+        zero_rows = row_sums == 0
+        self.A[~zero_rows] /= row_sums[~zero_rows, None]
+        self.A[zero_rows] = 0
+        self.A[self.A == 0] = self.epsilon
+        self.A = np.log10(self.A)
+
+        row_sums = self.B.sum(axis=1)
+        zero_rows = row_sums == 0
+        self.B[~zero_rows] /= row_sums[~zero_rows, None]
+        self.B[zero_rows] = 0
+        self.B[self.B == 0] = self.epsilon
+        self.B = np.log10(self.B)
+
+    def valid(self, valid_data):
+        return [self.viterbi(sentence) for sentence, _ in valid_data]
+
+    def viterbi(self, O):
+        N = len(self.Pi)
+        T = len(O)
+        delta = np.zeros((N, T))
+        psi = np.zeros((N, T), dtype=int)
+
+        delta[:, 0] = self.Pi + self.B[:, self.vocab[O[0]]]
+        for t in range(1, T):
+            O_t = self.vocab[O[t]]
+            for j in range(N):
+                # delta[j, t] = max(delta[i, t-1] * A[i, j] * B[j, O[t]]) 1<=i<=N
+                # 但是参数已经取过log了，所以这里得用加法
+                delta[j, t] = np.max(delta[:, t - 1] + self.A[:, j]) + self.B[j, O_t]
+                psi[j, t] = np.argmax(delta[:, t - 1] + self.A[:, j])
+
+        best_path = np.zeros(T, dtype=int)
+        best_path[-1] = np.argmax(delta[:, -1])
+        for t in range(T - 2, -1, -1):
+            best_path[t] = psi[best_path[t + 1], t + 1]
+
+        return [self.idx2tag[id] for id in best_path]
+
+    def save_param(self, filename, format="txt"):
+        if format == "txt":
+            np.savetxt(f"{filename}_A.txt", self.A)
+            np.savetxt(f"{filename}_B.txt", self.B)
+            np.savetxt(f"{filename}_Pi.txt", self.Pi)
+        elif format == "npy":
+            np.save(f"{filename}_A.npy", self.A)
+            np.save(f"{filename}_B.npy", self.B)
+            np.save(f"{filename}_Pi.npy", self.Pi)
+        elif format == "npz":
+            np.savez(f"{filename}.npz", A=self.A, B=self.B, Pi=self.Pi)
+
+    def load_param(self, filename, format="txt"):
+        if format == "txt":
+            self.A = np.loadtxt(f"{filename}_A.txt")
+            self.B = np.loadtxt(f"{filename}_B.txt")
+            self.Pi = np.loadtxt(f"{filename}_Pi.txt")
+        elif format == "npy":
+            self.A = np.load(f"{filename}_A.npy")
+            self.B = np.load(f"{filename}_B.npy")
+            self.Pi = np.load(f"{filename}_Pi.npy")
+        elif format == "npz":
+            loaded = np.load(f"{filename}.npz")
+            self.A = loaded["A"]
+            self.B = loaded["B"]
+            self.Pi = loaded["Pi"]
+
+    def predict(self, O):
+        results = self.viterbi(O)
+        for word, label in zip(O, results):
+            print(f"{word} {label}")
---
+++ Project2/Part1/dataprocess.py
@@ -0,0 +1,128 @@
+import re
+import os
+import sys
+import numpy as np
+import logging
+
+
+def build_vocab(file_paths):
+    vocab = {}
+    for file_path in file_paths:
+        with open(file_path, "r", encoding="utf-8") as file:
+            lines = file.readlines()
+            for line in lines:
+                line = line.strip()
+                if line:
+                    word, _ = line.split()  # 分割每行的单词和标签
+                    if word not in vocab:
+                        vocab[word] = len(vocab)
+    return vocab
+
+
+def build_tag2idx(filename):
+    tags = []
+    with open(filename, "r", encoding="utf-8") as f:
+        content = f.read()
+        tags = re.findall(r"[B|M|E|S|I]-[A-Z]+", content)
+    if filename == "../NER/Chinese/tag.txt":
+        tags = tags[4:]
+    else:
+        tags = tags[2:]
+    tags.append("O")
+    tag2idx = {}
+    tag2idx["O"] = 0
+    count = 1
+    for tag in tags[:-1]:
+        tag2idx[tag] = count
+        count += 1
+    return tag2idx
+
+
+def read_data(file_path):
+    sentences = []
+    labels = []
+    curr_sentence = []
+    curr_labels = []
+
+    with open(file_path, "r", encoding="utf-8") as file:
+        lines = file.readlines()
+        for line in lines:
+            line = line.strip()
+            if line == "":
+                if curr_sentence:
+                    sentences.append(curr_sentence)
+                    labels.append(curr_labels)
+                    curr_sentence = []
+                    curr_labels = []
+                continue
+            token, label = line.split(" ")
+            curr_sentence.append(token)
+            curr_labels.append(label)
+    if len(curr_sentence) > 0:
+        sentences.append(curr_sentence)
+        labels.append(curr_labels)
+
+    return sentences, labels
+
+
+def save_as_npz(sentences, labels, file_path):
+    array1 = np.array(sentences, dtype=object)
+    array2 = np.array(labels, dtype=object)
+    np.savez(file_path, sentences=array1, labels=array2)
+
+
+def load_from_npz(file_path):
+    loaded_data = np.load(file_path, allow_pickle=True)
+    sentences = loaded_data["sentences"]
+    labels = loaded_data["labels"]
+    return sentences, labels
+
+
+def data_process(folder_path):
+    train_file = os.path.join(folder_path, "train.npz")
+    valid_file = os.path.join(folder_path, "valid.npz")
+
+    if not os.path.isfile(train_file):
+        train_path = os.path.join(folder_path, "train.txt")
+        train_sentences, train_labels = read_data(train_path)
+        save_as_npz(train_sentences, train_labels, train_file)
+
+    if not os.path.isfile(valid_file):
+        valid_path = os.path.join(folder_path, "validation.txt")
+        valid_sentences, valid_labels = read_data(valid_path)
+        save_as_npz(valid_sentences, valid_labels, valid_file)
+
+    train_sentences, train_labels = load_from_npz(train_file)
+    valid_sentences, valid_labels = load_from_npz(valid_file)
+
+    logging.info(f"train dataset size: {len(train_sentences)}")
+    logging.info(f"valid dataset size: {len(valid_sentences)}")
+
+    train_data = list(zip(train_sentences, train_labels))
+    valid_data = list(zip(valid_sentences, valid_labels))
+    return train_data, valid_data
+
+
+def set_log(file_path):
+    for handler in logging.root.handlers[:]:
+        logging.root.removeHandler(handler)
+
+    log_handlers = [logging.StreamHandler(stream=sys.stdout)]
+    if file_path is not None:
+        log_handlers.append(logging.FileHandler(file_path))
+
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s P%(process)d %(levelname)s %(message)s",
+        handlers=log_handlers,
+    )
+
+
+def combine_data(sentences, labels):
+    combined_data = []
+    for sentence, label in zip(sentences, labels):
+        combined_tokens = [f"{token} {tag}\n" for token, tag in zip(sentence, label)]
+        combined_data.append("".join(combined_tokens))
+    combined_data = "\n".join(combined_data)
+    combined_data += "\n"
+    return combined_data
